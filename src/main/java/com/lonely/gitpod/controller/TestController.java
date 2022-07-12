a9bc81a286667051b484c0876bbe8914ee8d39d2
黄志标 <373008554@qq.com>
2022-07-12 16:01:29 +0800

    测试集成lombok插件功能

--- src/main/java/com/lonely/gitpod/controller/TestController.java
+++ src/main/java/com/lonely/gitpod/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.lonely.gitpod.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
 
 
     @GetMapping("/test1")
-    public String test1(){
+    public String test1() {
         return "test1";
     }
 
+    /**
+     * 输出日志，测试 gitpod集成插件功能
+     *
+     * @return
+     */
+    @GetMapping("/test2")
+    public String test2() {
+        log.info("调用了test2()");
+        return "test2";
+    }
 
 }
